961b0f34c71c0f6b063b14e3e962aec7e42acd62
L3 Squad <l3squad.com@gmail.com>
2025-01-11 12:38:10 +0800

    tech: add login client app

---
+++ client/login/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Components.
+import { ErrorHandler } from '@login/components'
+
+function App() {
+  return (
+    <>
+      <ErrorHandler>
+        <h1>Login Form</h1>
+      </ErrorHandler>
+    </>
+  )
+}
+
+export default App
